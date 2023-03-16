03a15190bada388fb0a764224c019ddcd0f546dc
SherrynneJoy <mbesambuvi@gmail.com>
2023-03-16 02:12:08 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - is the main function
+ * Return: 0 when it evaluates successfully
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
