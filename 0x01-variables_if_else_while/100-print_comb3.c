d77550a29ff62886aae726feb14ab62b0c523d5e
SherrynneJoy <mbesambuvi@gmail.com>
2023-03-17 20:18:08 -0700

    smallest combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - is the main function
+ * Return: 0 when it evaluates successfully
+ */
+
+int main(void)
+{
+	int i;
+	int m;
+
+	for (i = 0 ; i < 10; i++)
+	{
+		for (m = 0 ; m < 10 ; m++)
+		{
+			if (i < m && i != m)
+			{
+				putchar(i + '0');
+				putchar(m + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
