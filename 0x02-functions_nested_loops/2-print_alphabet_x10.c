1d3533995bb6b8fa59d79d830c83a89cb46603ff
SherrynneJoy <mbesambuvi@gmail.com>
2023-03-21 18:40:01 -0700

    is lower

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - is the function that prints the alphabet ten times
+ * Return: 0 always
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char m;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (m = 'a'; m <= 'z'; m++);
+		{
+			_putchar(m);
+		}
+	}
+	_putchar('\n');
+}
