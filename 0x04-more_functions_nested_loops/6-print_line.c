9209b5b22a62eaafa162ab809d37703c307b8f34
SherrynneJoy <mbesambuvi@gmail.com>
2023-03-23 09:10:32 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: the number of times the character _ should be printed
+ * Return: \n
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
