d2711eaa4fbe072d39e0e2eb18a68772d352bd24
SherrynneJoy <mbesambuvi@gmail.com>
2023-03-30 08:23:37 -0700

    concatenation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,31 @@
 #include "main.h"
 
 /**
- * *_strncat - concatenates strings
+ * _strncat - concatenates strings
  * @dest: the first string
  * @src: the second string
  * Return: concatenated string
+ * @n: an integer
  */
 
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
+{
+	int m;
+	int p;
+
+	m = 0;
+	while (dest[m] != '\0')
+	{
+		m++;
+	}
+	p = 0;
+	while (p < n && src[p] != '\0')
+	{
+	dest[m] = src[p];
+	m++;
+	p++;
+	}
+	dest[m] = '\0';
+	return (dest);
+}
 
