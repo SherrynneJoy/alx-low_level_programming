0938837da7b291be79e3d23d617a0faf2c2fccca
SherrynneJoy <mbesambuvi@gmail.com>
2023-03-30 07:47:15 -0700

    concatenate

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates strings
+ * @dest: the first string
+ * @src: the second string
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src, int n)
+
