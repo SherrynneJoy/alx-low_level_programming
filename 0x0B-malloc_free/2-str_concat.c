5fa849c8bce732ebcc3396c2f1984084cf71cd6b
SherrynneJoy <mbesambuvi@gmail.com>
2023-06-13 10:07:42 -0700

    concatenates strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 	int s1_length = strlen(s1);
 	int s2_length = strlen(s2);
 	int size = s1_length + s2_length + 1;
-	char *t = malloc(sizeof(char) * (size + 1));
+	char *t = malloc(sizeof(char) * (s1_length + s2_length + 1));
 	int i;
 
 	if (s1 == NULL)
