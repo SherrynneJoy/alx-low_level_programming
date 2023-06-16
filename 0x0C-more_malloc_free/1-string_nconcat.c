e2419bc16e9e8d1540f07de238123db14068ce03
SherrynneJoy <mbesambuvi@gmail.com>
2023-06-15 19:31:00 -0700

    array range

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates strings
+ * @s1: string one
+ * @s2: second string
+ * @n: an integer
+ * Return: pointer to allocated memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int s1_len = strlen(s1);
+	unsigned int s2_len = strlen(s2);
+	unsigned int i;
+
+	n = s1_len + s2_len + 1;
+	if (ptr == NULL)
+		return (NULL);
+	if (n < s2_len)
+	{
+		ptr = malloc(sizeof(char) * (s1_len + n + 1);
+	}
+	else
+	{
+	ptr = malloc(sizeof(char) * n);
+	}
+
+	for (i = 0; i < s1_len; i++)
+	ptr[i] = s1[i];
+
+	for (i = 0; i < s2_len; i++)
+	ptr(s1_len + i) = s2[i];
+	
+	ptr[s1_len + s2_len] = '\0';
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min: minimum integer
+ * @max: maximum integer
+ * Return: 0 when it evaluates
+ */
+
+int *array_range(int min, int max)
+{
+	int *s;
+	int size;
+	int i;
+
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+	s = malloc(sizeof(int *) * size);
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++)
+		s[i] = min++;
+
+	return (s);
+}
+
