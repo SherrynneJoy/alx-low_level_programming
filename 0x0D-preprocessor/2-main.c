3f85a97239fd223300a051f9846948a610281c67
SherrynneJoy <mbesambuvi@gmail.com>
2023-06-19 08:53:52 -0700

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints the file path as a string
+ * Return: nothing
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return(0);
+}
