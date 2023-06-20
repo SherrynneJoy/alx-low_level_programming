dddcbca9dbe698c412294b5f0f86d2e1771d621d
SherrynneJoy <mbesambuvi@gmail.com>
2023-06-19 19:16:53 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,6 +7,8 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: person who the dog belongs to
+ *
+ * Description: this structure shows the characteristics of a dog
  */
 
 struct dog {
